d8cf96fe4a820a6fa2c1114d4d98d71e0a8e1a7a
VPolazau <VPolazau@gmail.com>
2023-04-08 13:34:55 +0300

    ErrorIndicator change

--- src/components/ErrorIndicator/ErrorIndicator.js
+++ src/components/ErrorIndicator/ErrorIndicator.js
@@ -1,7 +1,23 @@
 import React from 'react';
 
+import styles from './styles.module.css';
+
 const ErrorIndicator = () => {
-  return <div>Error!</div>;
+  return (
+    <div className={styles.error_container}>
+      <h1>Sorry, something went wrong.</h1>
+      <br />
+      <span>We are already trying to solve this problem.</span>
+      <br />
+      <br />
+      <br />
+      <h3>Here are some helpful links:</h3>
+      <ul>
+        <li><a href='#'>Back</a></li>
+        <li><a href='#'>Home</a></li>
+      </ul>
+    </div>
+  );
 };
 
 export { ErrorIndicator };
