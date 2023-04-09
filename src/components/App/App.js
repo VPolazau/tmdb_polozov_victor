dc2afe2e4ec75e533ea349024970d5503764391e
VPolazau <VPolazau@gmail.com>
2023-04-09 11:54:22 +0300

    Film

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { Header } from '../Header';
 import { ItemList } from '../ItemList';
+// import { Film } from '../Film'
+// import { Person } from '../Person'
 
-import styles from './styles.module.css';
+import {TMDBService} from '../../service/TMDBService'
+const tmdbService = new TMDBService()
+
 
 const App = () => {
+  const [item, setItem] = useState()
+
+  useEffect(() => {
+    // tmdbService.getPerson(224513).then(res => setItem(res))
+    // tmdbService.getMovie(677179).then(res => setItem(res))
+    // tmdbService.getMovie(76600).then(res => setItem(res))
+    // tmdbService.getFilms('popular', 1).then((res) => console.log(res.results));
+    // tmdbService.getPeople(1).then((res) => console.log(res.results));
+  }, [])
+  
   return (
-    <div className={styles.container}>
+    <>
       <Header />
+      {/* {item && <Person item={item}/>} */}
+      {/* {item && <Film item={item}/>} */}
       <ItemList />
-    </div>
+    </>
   );
 };
 
---
+++ src/components/Film/Film.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Rating from '@mui/material/Rating';
+import StarIcon from '@mui/icons-material/Star';
+
+import styles from './styles.module.css';
+
+const Film = ({ item }) => {
+  const {
+    title,
+    vote_average,
+    genres,
+    poster_path,
+    release_date,
+    runtime,
+    overview,
+    backdrop_path,
+    production_countries,
+    status,
+  } = item;
+
+  const infoView = (type) =>{
+    return type.map((i, idx) => {
+      if (!idx) return <span key={i?.id || idx}>{i.name}</span>;
+      return <span key={i?.id || idx}>, {i.name}</span>;
+    });}
+
+  return (
+    <div
+      style={{
+        backgroundImage: `url(https://image.tmdb.org/t/p/original/${backdrop_path})`,
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+      }}
+    >
+      <div className={styles.film_container}>
+        <div className={styles.poster_wrapper}>
+          <div className={styles.poster}>
+            <img
+              className={styles.film_image}
+              src={`https://image.tmdb.org/t/p/w300_and_h450_bestv2${poster_path}`}
+              alt={title}
+              srcSet={`https://image.tmdb.org/t/p/w300_and_h450_bestv2${poster_path} 1x, https://image.tmdb.org/t/p/w600_and_h900_bestv2${poster_path} 2x`}
+            />
+          </div>
+        </div>
+        <div className={styles.film_info}>
+          <h2 className={styles.title}>
+            {title}
+            <span className={styles.tag}>{` (${release_date.slice(0, 4)})`}</span>
+          </h2>
+          <div className={styles.facts}>
+            <span className={styles.release}>{release_date.split('-').join('/')}</span>
+            <span>{'\xa0 • \xa0'}</span>
+            <span className={styles.genres}>{infoView(genres)}</span>
+            <span>{'\xa0 • \xa0'}</span>
+            <span className={styles.runtime}>{runtime}m</span>
+          </div>
+          <span className={styles.countries}>
+            <span className={styles.status_tag}>Countries: </span>
+            {infoView(production_countries)}
+          </span>
+          <span className={styles.status}>
+            <span className={styles.status_tag}>Status: </span>
+            {status}
+          </span>
+          <Box sx={{ display: 'flex' }}>
+            <Rating name='size-small' value={vote_average / 2} precision={0.1} size='large' readOnly/>
+            <Typography
+              variant='caption'
+              sx={{ pt: '3px', fontSize: '16px', opacity: 0.8, userSelect: 'none' }}
+            >{`\xa0${vote_average}`}</Typography>
+          </Box>
+          <div className={styles.info_body}>
+            <h3 className={styles.auto}>Overview</h3>
+            <div className={styles.overview}>
+              <p>{overview}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export { Film };
