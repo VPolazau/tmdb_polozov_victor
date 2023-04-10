d7a0bd160abaaa3774b43f3cbe5a9ea063531df1
VPolazau <VPolazau@gmail.com>
2023-04-10 20:16:25 +0300

    no_image

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -4,6 +4,7 @@ import Typography from '@mui/material/Typography';
 import Rating from '@mui/material/Rating';
 
 import styles from './styles.module.css';
+import no_image from './no_image.svg';
 
 const Item = ({ item }) => {
   const { vote_average, title, release_date, poster_path, name, profile_path } = item;
@@ -16,24 +17,39 @@ const Item = ({ item }) => {
             <img
               loading='lazy'
               className={styles.poster}
-              src={`https://image.tmdb.org/t/p/w220_and_h330_face${poster_path || profile_path}`}
-              srcSet={`https://image.tmdb.org/t/p/w220_and_h330_face${poster_path || profile_path} 1x, https://image.tmdb.org/t/p/w440_and_h660_face${poster_path || profile_path} 2x`}
-              alt={title || name}
+              src={
+                !poster_path && !profile_path
+                  ? no_image
+                  : `https://image.tmdb.org/t/p/w220_and_h330_face${poster_path || profile_path}`
+              }
+              srcSet={
+                !poster_path && !profile_path
+                  ? no_image
+                  : `https://image.tmdb.org/t/p/w220_and_h330_face${
+                      poster_path || profile_path || no_image
+                    } 1x, https://image.tmdb.org/t/p/w440_and_h660_face${poster_path || profile_path || no_image} 2x`
+              }
+              alt={title || name || undefined}
             />
           </a>
         </div>
       </div>
       <div className={styles.content}>
-        {vote_average && <Box sx={{ display: 'flex' }}>
-          <Rating name='size-small' value={vote_average / 2} precision={0.1} size='small' readOnly />
-          <Typography variant='caption' sx={{ color: 'gray', userSelect: 'none' }}>{`\xa0${vote_average}`}</Typography>
-        </Box>}
+        {(vote_average || vote_average !== 0) && (
+          <Box sx={{ display: 'flex' }}>
+            <Rating name='size-small' value={vote_average / 2} precision={0.1} size='small' readOnly />
+            <Typography
+              variant='caption'
+              sx={{ color: 'gray', userSelect: 'none' }}
+            >{`\xa0${vote_average}`}</Typography>
+          </Box>
+        )}
         <h2 className={styles.content_title}>
           <a href='#flor' title={title || name}>
-            {title || name}
+            {title || name || undefined}
           </a>
         </h2>
-        {release_date && <p className={styles.content_date}>{release_date}</p>}
+        {release_date && <p className={styles.content_date}>{release_date.split('-').reverse().join('/')}</p>}
       </div>
     </div>
   );
